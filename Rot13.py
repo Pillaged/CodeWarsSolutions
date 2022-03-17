9ce68e97b6990458e81af6bb93cbf68c1759ef62
Josiah Luna <josiah.luna@live.com>
2022-03-16 21:11:37 -0700

    Update Rot13.py

--- Rot13.py
+++ Rot13.py
@@ -1,1 +1,10 @@
-
+def rot13(message):
+    m2 = ""
+    for char in message:
+        pholder = ord(char)
+        if 122>= pholder >= 110 or 90>=pholder>=78:
+            pholder -= 13
+        elif 110>pholder>=97 or 78>pholder>=65:
+            pholder += 13
+        m2 += (chr(pholder))
+    return m2
